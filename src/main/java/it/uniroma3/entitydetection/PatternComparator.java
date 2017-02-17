117acad227e302a04ceeeddc1e23a99e24c5f9eb
matteo <mcannaviccio@gmail.com>
2017-02-17 15:33:53 +0100

    changed PatternComparator solving the bug with equal entries

--- src/main/java/it/uniroma3/entitydetection/PatternComparator.java
+++ src/main/java/it/uniroma3/entitydetection/PatternComparator.java
@@ -1,20 +1,25 @@
 package it.uniroma3.entitydetection;
 
 import java.util.Comparator;
+
+import it.uniroma3.util.Pair;
 /**
  * 
  * @author matteo
  *
  */
-public class PatternComparator implements Comparator<String> {
+public class PatternComparator implements Comparator<Pair<String, String>> {
 
     @Override
-    public int compare(String o1, String o2) {
-	o1 = o1.replaceAll("\\(.*?\\)", "");
-	o2 = o2.replaceAll("\\(.*?\\)", ""); 
-	if (o1 == null || o2 == null)
-	    return 0;
-	return o2.length() - o1.length();
+    public int compare(Pair<String, String> p1, Pair<String, String> p2) {
+	String o1 = p1.key.replaceAll("\\(.*?\\)", "");
+	String o2 = p2.key.replaceAll("\\(.*?\\)", ""); 
+	if (o1.length() > o2.length()) {
+	    return -1;
+	} else if (o1.length() < o2.length()) {
+	    return 1;
+	}
+	return o1.compareTo(o2);
     }
 
 
