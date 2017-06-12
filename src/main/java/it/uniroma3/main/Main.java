a0e01b3f2dad7b208f1c42c8eb74bf2533e19ff9
matteo <mcannaviccio@gmail.com>
2017-06-12 22:08:20 +0200

    change order of languages

--- src/main/java/it/uniroma3/main/Main.java
+++ src/main/java/it/uniroma3/main/Main.java
@@ -28,7 +28,7 @@ public class Main {
      * @param args
      */
     public static void main(String[] args){
-	String[] languages = new String[]{"en", "es", "it", "fr", "de"};
+	String[] languages = new String[]{"it", "es", "fr", "de", "en"};
 	for (String lang : languages){
 	    Configuration.init(args);
 	    Configuration.setParameter("language", lang);
