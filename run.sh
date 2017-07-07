093379c02260a604fb60c88139d4c1832873b146
Matteo Cannaviccio <mcannaviccio@gmail.com>
2017-07-07 17:40:53 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -jar target/lectorplus-1.0-SNAPSHOT-jar-with-dependencies.jar config.properties dataFile=data outputFolder=$1
+java -jar target/lectorplus-1.0-SNAPSHOT-jar-with-dependencies.jar config.properties dataFile=data outputFolder=$1 useSpotlight=$2
